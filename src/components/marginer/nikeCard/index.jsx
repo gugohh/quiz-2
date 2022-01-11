0adce1a4df8a841b3e10c195a2bbaf67c40f75f1
gugohh <oohhgug@gmail.com>
2022-01-11 19:03:49 +0100

    working on nikeCard component

--- src/components/marginer/nikeCard/index.jsx
+++ src/components/marginer/nikeCard/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+
+const CardContainer = styled.div`
+    width: 285px;
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 25px;
+    box-shadow: 0 2px 7px 1px rgba(31, 31, 31, 0.2);
+    background-color: #1d1f21;
+    color: #fff;
+    position: relative;
+    cursor: grab;
+`;
+
 
 const nikeCard = () => {
 
     return (
-        <div>
-            
-        </div>
+       <CardContainer>
+       
+       </CardContainer>
     )
 }
 
